e1561e73e5085c8fc098e6d1aca4be1c02128934
Matthew Brown <brownm18@rpi.edu>
2021-04-27 22:00:11 -0400

    Comparing Live Data using Distance Algorithm

--- Final Project Combined Work/Assets/Project/Scripts/Distance_Algorithm/Distance_Algorithm_Manager.cs
+++ Final Project Combined Work/Assets/Project/Scripts/Distance_Algorithm/Distance_Algorithm_Manager.cs
@@ -11,18 +11,29 @@ public class Distance_Algorithm_Manager : MonoBehaviour
     private Dictionary<string, int> inertia = new Dictionary<string, int>();
 
     public Database_Manager DM;
+
+    public bool live_data = false;
     public HandPlayback HP;
+    public HandRecorder recorder;
 
     private Dictionary<string, List<double>> likeness_score = new Dictionary<string, List<double>>();
 
     internal string current_classified_motion = "NA";
-    public double tolerance = 10000000000;
+
+    public int live_data_frame_rate = 90;
+    public double distance_algorithm_tolerance = 10000000000;
+    public float deadTimeMovementThreshhold = .025f;
+
+    public int number_of_frames_looked_at = 100;
+
+    public float seconds_between_live_data_checks = 1;
+    private float next_live_data_check_time = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("Actual_Start", 1);
+        Invoke("Actual_Start", .1f);
     }
 
 
@@ -32,23 +43,74 @@ public class Distance_Algorithm_Manager : MonoBehaviour
         foreach (string motion_name in DM.formatters.Keys) {
             foreach (Database_Input_Formatter DIF in DM.formatters[motion_name]) {
                 DIF.solve_for_energy(inertia);
-                // break;
             }
         }
 
+        if (!live_data) {
+            double live_left_hand_energy = solve_hand_energy(HP.leftFrameData, "lhand");
+            double live_right_hand_energy = solve_hand_energy(HP.rightFrameData, "rhand");
+            List<double> live_energy = new List<double>() { live_left_hand_energy, live_right_hand_energy };
 
-        double live_left_hand_energy = solve_hand_energy(HP.leftFrameData, "lhand");
-        double live_right_hand_energy = solve_hand_energy(HP.rightFrameData, "rhand");
-        List<double> live_energy = new List<double>() { live_left_hand_energy, live_right_hand_energy };
+            Debug.Log("live_left_hand_energy: " + live_left_hand_energy);
+            Debug.Log("live_right_hand_energy: " + live_right_hand_energy);
 
-        Debug.Log("live_left_hand_energy: " + live_left_hand_energy);
-        Debug.Log("live_right_hand_energy: " + live_right_hand_energy);
-
-        distance_algorithm(live_energy);
-        current_classified_motion = classified_motion();
-        Debug.Log(current_classified_motion);
+            distance_algorithm(live_energy);
+            current_classified_motion = classified_motion();
+            Debug.Log("Classified Motion: " + current_classified_motion);
+        }
+        
     }
-    
+
+    void FixedUpdate() {
+        if (live_data && Time.time >= next_live_data_check_time) {
+            Debug.Log("Check Live Data");
+            next_live_data_check_time = Time.time + seconds_between_live_data_checks;
+
+
+            List<FrameData> leftFrameData, rightFrameData;
+            (leftFrameData, rightFrameData) = get_live_data();
+
+            if (leftFrameData == null || rightFrameData == null) {
+                Debug.Log("Invalid leftFrameData or rightFrameData data");
+                return;
+            }
+
+            double live_left_hand_energy = solve_hand_energy(leftFrameData, "lhand");
+            double live_right_hand_energy = solve_hand_energy(rightFrameData, "rhand");
+
+            Debug.Log("live_left_hand_energy: " + live_left_hand_energy);
+            Debug.Log("live_right_hand_energy: " + live_right_hand_energy);
+
+            distance_algorithm(new List<double>() { live_left_hand_energy, live_right_hand_energy });
+            current_classified_motion = classified_motion();
+            Debug.Log("Classified Motion: " + current_classified_motion);
+        }
+    }
+
+    // Get Live Data
+    public (List<FrameData>, List<FrameData>) get_live_data() {
+        List<FrameData> leftFrameData = recorder.leftFrameData;
+        List<FrameData> rightFrameData = recorder.rightFrameData;
+
+        if (leftFrameData.Count < number_of_frames_looked_at || rightFrameData.Count < number_of_frames_looked_at) {
+            return (null, null);
+        }
+
+        leftFrameData = (List <FrameData>) leftFrameData.Skip(leftFrameData.Count() - number_of_frames_looked_at);
+        rightFrameData = (List<FrameData>) rightFrameData.Skip(leftFrameData.Count() - number_of_frames_looked_at);
+
+        leftFrameData = ClearDeadTime(leftFrameData);
+        rightFrameData = ClearDeadTime(rightFrameData);
+
+        /*
+        if (leftFrameData.Count < 50 || rightFrameData.Count < 50) {
+            return (null, null);
+        }
+        */
+
+        return (leftFrameData, rightFrameData);
+    }
+
     // Getting Hand Energy
     public double solve_hand_energy(List<FrameData> frame_data, string bone_name) {
 
@@ -72,12 +134,43 @@ public class Distance_Algorithm_Manager : MonoBehaviour
             Vector3 v = x_1 - x_0;
 
             // TO DO: TIME BETWEEN FRAMES
-            v *= 90; // Refresh rate (Sampling rate) of system
+            v *= live_data_frame_rate; // Refresh rate (Sampling rate) of system
 
             output.Add(v);
         }
 
         return output;
+    }
+
+    // Normalize Data
+    private List<FrameData> ClearDeadTime(List<FrameData> frameData) {
+        List<FrameData> newFrameData = new List<FrameData>();
+        int minI = 0;
+        int maxI = 0;
+        for (int i = 0; i < frameData.Count - 1; i++) {
+            float delta = (frameData[i].position - frameData[i + 1].position).magnitude;
+            if (delta > deadTimeMovementThreshhold) {
+                minI = i;
+                break;
+            }
+        }
+        Debug.Log("here1");
+        for (int i = frameData.Count - 1; i > 0; i--) {
+            float delta = (frameData[i].position - frameData[i - 1].position).magnitude;
+            if (delta > deadTimeMovementThreshhold) {
+                maxI = i;
+                break;
+            }
+        }
+        Debug.Log("min i = " + minI);
+        Debug.Log("max i = " + maxI);
+        Debug.Log("count = " + frameData.Count);
+        for (int i = minI; i <= maxI; i++) {
+            FrameData fd = new FrameData();
+            fd.position = frameData[i].position;
+            newFrameData.Add(fd);
+        }
+        return newFrameData;
     }
 
     // Distance Algorithm
@@ -109,7 +202,7 @@ public class Distance_Algorithm_Manager : MonoBehaviour
 
     public string classified_motion() {
         string output = "NA";
-        double lowest_score = tolerance;
+        double lowest_score = distance_algorithm_tolerance;
 
         foreach (string motion_name in likeness_score.Keys) {
 
@@ -132,7 +225,6 @@ public class Distance_Algorithm_Manager : MonoBehaviour
 
         return output;
     }
-
 
     // Helper Functions
     void fill_inertia() {
